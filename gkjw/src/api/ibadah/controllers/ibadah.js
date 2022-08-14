2cb1f7497fe9987a90dc820074908470e99d65b9
Julius-745 <54503854+Julius-745@users.noreply.github.com>
2022-08-14 13:59:00 +0700

    add ibadah table

---
+++ gkjw/src/api/ibadah/controllers/ibadah.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  ibadah controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::ibadah.ibadah');
---
+++ gkjw/src/api/ibadah/services/ibadah.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ibadah service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::ibadah.ibadah');
